dc35c72e507325e689d31730b2d4c416386bb15f
sankalpjumale <sankalpjumale@gmail.com>
2026-06-18 23:41:01 +0530

    feat: add TabEmpty component

---
+++ components/policy_detail_tabs/TabEmpty.tsx
@@ -0,0 +1,7 @@
+export function TabEmpty({ text }: { text: string }) {
+  return (
+    <div className="flex items-center justify-center py-16 text-stone-400 text-sm">
+      {text}
+    </div>
+  )
+}
