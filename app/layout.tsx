25a30c44bce899efee03ddfa026b319477bd236a
sankalpjumale <sankalpjumale@gmail.com>
2026-06-30 23:30:27 +0530

    fix: unused file

--- app/layout.tsx
+++ app/layout.tsx
@@ -30,7 +30,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    // <ClerkProvider>
+    <ClerkProvider>
       <html
         lang="en"
         className={cn("h-full", "antialiased", geistSans.variable, geistMono.variable, "font-sans", inter.variable)}
